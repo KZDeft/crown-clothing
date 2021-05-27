54902f88d99811449f41612cb1cebdfa2078a9f2
KZDeft <ingeni0usvn@gmail.com>
2021-05-28 00:20:34 +0700

    fix firebase and button google styles

--- src/App.js
+++ src/App.js
@@ -5,18 +5,39 @@ import HomePage from "./pages/homepage/hompage.component";
 import ShopPage from "./pages/shoppage/shoppage.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
+import { auth } from "./components/firebase/firebase.utils";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+  unsubcribeFromAuth = null;
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin/" component={SignInAndSignUp} />
-      </Switch>
-    </div>
-  );
+  componentDidMount() {
+    this.unsubcribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubcribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin/" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
